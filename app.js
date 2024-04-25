4abb2c135cac93f6fc5f520e81abd040d2dceb8f
IngDavidTM <david-1011@hotmail.com>
2024-04-25 10:22:17 -0500

    Add v-if and v-else to show information

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
+      enteredValue: '',
       goals: []
     };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
 });
 
 app.mount('#user-goals');
